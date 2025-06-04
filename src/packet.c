8224ff743f2452a96059cf089c1c1b5dd01f0d95
Maksym Trehubenko <maks.trehubenko@gmail.com>
2025-06-04 05:45:36 +0300

    refactor unit tests (test per file)

--- src/packet.c
+++ src/packet.c
@@ -2,6 +2,7 @@
 #include <mctp/util/alloc.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 mctp_packet_t *mctp_pkt_create(
     const mctp_transport_header_t *header,
@@ -41,4 +42,18 @@ bool mctp_pkt_message_match(
         packet->header.source == message_ctx->eid &&
         packet->header.tag == message_ctx->tag &&
         packet->header.tag_owner == message_ctx->tag_owner;
+}
+
+void mctp_pkt_header_dump(
+    const mctp_transport_header_t *header
+) {
+    printf("MCTP Transport header\n");
+    printf("version:    0x%X\n",    header->version);
+    printf("dest:       0x%02X\n",  header->dest);
+    printf("source:     0x%02X\n",  header->source);
+    printf("tag:        0x%d\n",    header->tag);
+    printf("tag_owner:  0x%s\n",    header->tag_owner ? "YES" : "NO");
+    printf("pkt_seq:    0x%d\n",    header->pkt_seq);
+    printf("som:        0x%s\n",    header->som ? "YES" : "NO");
+    printf("eom:        0x%s\n",    header->eom ? "YES" : "NO");
 }
