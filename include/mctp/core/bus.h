2692ce3a2a443995ea755e98ed656c5dc2af628e
Maksym Trehubenko <maks.trehubenko@gmail.com>
2025-07-30 23:14:19 +0300

    cut project files

--- include/mctp/core/bus.h
+++ include/mctp/core/bus.h
@@ -1,45 +1,4 @@
 #ifndef _MCTP_CORE_BUS_H_
 #define _MCTP_CORE_BUS_H_
 
-#include <mctp/core/base.h>
-#include <mctp/core/binding.h>
-#include <mctp/core/packet_queue_map.h>
-#include <mctp/core/message_queue.h>
-#include <mctp/util/extern_c.h>
-
-
-typedef struct mctp_bus_t
-{
-	mctp_eid_t eid;
-    mctp_binding_t *binding;
-    struct
-    {
-        mctp_pktq_map_t pktq_map;
-        mctp_msgq_t msg_queue;
-    }
-    rx;
-}
-mctp_bus_t;
-
-
-EXTERN_C_BEGIN
-
-mctp_bus_t *mctp_bus_create();
-
-void mctp_bus_destroy(
-    mctp_bus_t *bus
-);
-
-void mctp_bus_set_eid(
-    mctp_bus_t *bus,
-    const mctp_eid_t eid
-);
-
-void mctp_bus_transport_bind(
-    mctp_bus_t *bus,
-    mctp_binding_t *binding
-);
-
-EXTERN_C_END
-
 #endif // _MCTP_CORE_BUS_H_
--- include/mctp/core/mctp.h
+++ include/mctp/core/mctp.h
@@ -1,47 +1,4 @@
 #ifndef _MCTP_CORE_MCTP_H_
 #define _MCTP_CORE_MCTP_H_
 
-#include <mctp/core/bus.h>
-#include <mctp/util/extern_c.h>
-
-#define MCTP_PKT_HDR_VER 0b0001
-
-
-EXTERN_C_BEGIN
-
-uint8_t mctp_get_message_tag();
-
-void mctp_message_disassemble(
-    const mctp_bus_t *bus,
-    const mctp_message_t *message,
-    mctp_pktq_t *tx_queue
-);
-
-void mctp_message_assemble(
-    mctp_message_t *message,
-    const mctp_pktq_t *rx_queue
-);
-
-void mctp_pktq_drain(
-    mctp_pktq_t *tx_queue,
-    const mctp_bus_t *bus
-);
-
-void mctp_pktq_tx(
-    const mctp_pktq_t *tx_queue,
-    const mctp_bus_t *bus
-);
-
-void mctp_packet_tx(
-    const mctp_bus_t *bus,
-    const mctp_packet_t *packet
-);
-
-void mctp_packet_rx(
-    mctp_bus_t *bus,
-    const mctp_packet_t *packet
-);
-
-EXTERN_C_END
-
 #endif // _MCTP_CORE_MCTP_H_
